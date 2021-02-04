b0857d0e2ce0c69eed84f1f173c2dfb50d96ee36
Wouter <wouter.masmeyer@live.nl>
2021-02-04 08:45:32 +0100

    adding js and author

---
+++ Basisprojecten/Javascript/lab01.js
@@ -0,0 +1,20 @@
+<html lang="nl">
+<head>
+ <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <meta name="Author" content="Wouter van Loozenoord">
+    Auteur: wm3214
+    <title> variabelen </title>
+</head>
+<body>
+
+<script>
+
+
+  </script>
+
+</body>
+</html>
